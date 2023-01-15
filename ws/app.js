0aaee7840348acbaae994902c076fe9df374623c
Samuel Logan <samuelogan987@gmail.com>
2023-01-15 18:44:51 +0000

    Initial commit

--- ws/app.js
+++ ws/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '26a27233d0724741872b854b998d31ac'; // Your client id
+var client_secret = 'db255c2bb904407196632201621e3750'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
